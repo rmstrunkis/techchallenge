f20d9fb59880e7c2c1485459b385109819d61c1c
Arlei <arlei_lepiani@hotmail.com>
2023-06-23 18:51:38 -0300

    feature: Ajuste de duplicados e swagger

--- src/main/java/com/fiap/techchallenge/pessoa/dto/PessoaDTO.java
+++ src/main/java/com/fiap/techchallenge/pessoa/dto/PessoaDTO.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fiap.techchallenge.pessoa.model.Pessoa;
 import com.fiap.techchallenge.pessoa.model.PessoaParentesco;
 import com.fiap.techchallenge.pessoa.model.PessoaSexo;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import org.hibernate.validator.constraints.br.CPF;
 
@@ -15,30 +17,46 @@ import javax.validation.constraints.Past;
 import java.time.LocalDate;
 
 @Getter
+@ApiModel(description = "Representa um objeto de requisicao de pessoas")
 public class PessoaDTO {
+
     @JsonIgnore
     private Long id;
+
+    @ApiModelProperty(value = "Informacao do ID do Usuário ao qual a pessoa estará vinculada", example = "1", position = 1)
     @NotNull(message = "Usuário é Obrigatório")
     private Long idUsuario;
+
+    @ApiModelProperty(value = "Informacao do CPF da pessoa", example = "57566310038", position = 1)
     @CPF(message = "CPF deve ser válido")
     @NotBlank(message = "CPF é Obrigatório")
     private String cpf;
+
+    @ApiModelProperty(value = "Informacao do Nome da pessoa", example = "Zezinho", position = 1)
     @NotBlank(message = "Nome é Obrigatório")
     private String nome;
+    @ApiModelProperty(value = "Informacao do Nome do Pai da pessoa", example = "Ze", position = 1)
     private String nomePai;
+    @ApiModelProperty(value = "Informacao do Nome da Mae da pessoa", example = "Zinha", position = 1)
     private String nomeMae;
+    @ApiModelProperty(value = "Informacao do telefone da pessoa", example = "11 3258-5303", position = 1)
     private String telefone;
+    @ApiModelProperty(value = "Informacao da senha da pessoa", example = "123", position = 1)
     @NotBlank(message = "Senha é Obrigatória")
     private String senha;
+    @ApiModelProperty(value = "Informacao da data de nascimento da pessoa", example = "01/01/2000", position = 1)
     @Past(message = "Data de nascimento deve ser maior que a Data Atual")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate dataNascimento;
+    @ApiModelProperty(value = "Informacao do email da pessoa", example = "xs@hotmail.com", position = 1)
     @Email(message = "Email deve ser válido")
     private String email;
 
+    @ApiModelProperty(value = "Informacao do Parentesco da pessoa com, o usuario", example = "FILHOS", position = 1)
     @NotNull(message = "Parentesco deve ser informado")
     private PessoaParentesco parentesco;
 
+    @ApiModelProperty(value = "Informacao do Sexo da pessoa", example = "MASCULINO", position = 1)
     @NotNull(message = "Sexo deve ser informado")
     private PessoaSexo sexo;
 
